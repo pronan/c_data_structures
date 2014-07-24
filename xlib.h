97e4f23ff5f1e5eaa8a471a8bb23402b4815b447
zhuanyenan <280145668@qq.com>
2014-07-24 14:19:24 +0800

    Create xlib.h
    Main header file.

---
+++ xlib.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <assert.h>
+typedef void PyObject;
+#include "dict.h"
+#include "list.h"
+#include "set.h"
