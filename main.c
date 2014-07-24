3484f7bed002794858592f77a41105ab09a2322d
zhuanyenan <280145668@qq.com>
2014-07-24 14:17:42 +0800

    Create main.c
    Test cases.

---
+++ main.c
@@ -0,0 +1,34 @@
+#include "xlib.h"
+
+/*scan words from stdin, print total amount for each word by DESC order*/
+int main(void) {
+    PyDictObject *mp = dict_new_custom(10000, 0, 0, 0, 0, 0, 0, 0);
+    //PyDictObject *mp = dict_new();
+    char keybuf[100];
+    size_t valuebuf[] = { 1 };
+    size_t *vp;
+    while (fscanf(stdin, "%s", keybuf) == 1) {
+        if (dict_contain(mp, keybuf)) {
+            vp = dict_search(mp, keybuf);
+            *vp += 1;
+        } else
+            dict_add(mp, keybuf, valuebuf);
+    }
+//    this is another faster version
+//    while (fscanf(stdin, "%s", keybuf) == 1) {
+//        vp = dict_force_search(mp, keybuf);
+//        *vp += 1;
+//    }
+//    dict_del(mp, "the");
+//    *valuebuf=123456789;
+//    dict_add(mp,"the",valuebuf);
+//    *valuebuf=1456789;
+//    dict_update(mp,"we",valuebuf);
+//    dict_del(mp, "and");
+//    dict_del(mp, "of");
+    dict_print_by_value_desc(mp);
+    dict_clear(mp);
+    dict_clear(mp); //just for test
+    free(mp);
+    return 0;
+}
