c800a14d9f18f8abecb0615e07c5025fe071d889
zhuanyenan <280145668@qq.com>
2014-07-24 23:07:05 +0800

    Update dict.c
    delete function "dict_contain", simplify names by deleting prefix "ma_" and "me_" and shorten some function names(such as "dict_search" to "dict_get", "dict_force_search" to "dict_fget")

--- dict.c
+++ dict.c
@@ -5,7 +5,7 @@ static PyDictEntry _dummy_struct;
 #define dummy (&_dummy_struct)
 
 static size_t
-keyhash(PyObject *_key) {
+default_keyhash(PyObject *_key) {
     char *key = (char *)_key;
     size_t hash = 5381;
     for (; *key; key++)
@@ -14,24 +14,24 @@ keyhash(PyObject *_key) {
 }
 
 static int
-keycmp(PyObject *key1, PyObject *key2) {
+default_keycmp(PyObject *key1, PyObject *key2) {
     return strcmp((char *)key1, (char *)key2);
 }
 
 static PyObject *
-keydup(PyObject *key) {
+default_keydup(PyObject *key) {
     return (PyObject *)strdup((char *)key);
 }
 
 static PyObject *
-valuedup(PyObject *_value) {
+default_valuedup(PyObject *_value) {
     size_t *value = (size_t*)malloc(sizeof(size_t));
     *value = *(size_t *)_value;
     return (PyObject *)value;
 }
 
 static PyObject *
-get_default_value(void) {
+default_valuedefault(void) {
     size_t *value = (size_t*)malloc(sizeof(size_t));
     *value = 0;
     return (PyObject *)value;
@@ -43,31 +43,31 @@ lookdict(PyDictObject *mp, PyObject *key, size_t hash) {
     size_t i;
     size_t perturb;
     PyDictEntry *freeslot;
-    size_t mask = mp->ma_mask;
-    PyDictEntry *ep0 = mp->ma_table;
+    size_t mask = mp->mask;
+    PyDictEntry *ep0 = mp->table;
     PyDictEntry *ep;
     i = (size_t)hash & mask;
     ep = &ep0[i];
-    if (ep->me_key == NULL || ep->me_key == key)
+    if (ep->key == NULL || ep->key == key)
         return ep;
-    if (ep->me_key == dummy)
+    if (ep->key == dummy)
         freeslot = ep;
-    else if (ep->me_hash == hash
-             && mp->ma_keycmp(ep->me_key, key) == 0)
+    else if (ep->hash == hash
+             && mp->keycmp(ep->key, key) == 0)
         return ep;
     else
         freeslot = NULL;
     for (perturb = hash;; perturb >>= PERTURB_SHIFT) {
         i = (i << 2) + i + perturb + 1;
         ep = &ep0[i & mask];
-        if (ep->me_key == NULL)
+        if (ep->key == NULL)
             return freeslot == NULL ? ep : freeslot;
-        if (ep->me_key == key
-                || (ep->me_hash == hash
-                    && ep->me_key != dummy
-                    && mp->ma_keycmp(ep->me_key, key) == 0))
+        if (ep->key == key
+                || (ep->hash == hash
+                    && ep->key != dummy
+                    && mp->keycmp(ep->key, key) == 0))
             return ep;
-        if (ep->me_key == dummy && freeslot == NULL)
+        if (ep->key == dummy && freeslot == NULL)
             freeslot = ep;
     }
     assert(0);          /* NOT REACHED */
@@ -79,21 +79,21 @@ static PyDictEntry *
 lookdict_nodummy(PyDictObject *mp, PyObject *key, size_t hash) {
     size_t i;
     size_t perturb;
-    size_t mask = mp->ma_mask;
-    PyDictEntry *ep0 = mp->ma_table;
+    size_t mask = mp->mask;
+    PyDictEntry *ep0 = mp->table;
     PyDictEntry *ep;
     i = (size_t)hash & mask;
     ep = &ep0[i];
-    if (ep->me_key == NULL
-            || ep->me_key == key
-            || (ep->me_hash == hash && mp->ma_keycmp(ep->me_key, key) == 0))
+    if (ep->key == NULL
+            || ep->key == key
+            || (ep->hash == hash && mp->keycmp(ep->key, key) == 0))
         return ep;
     for (perturb = hash;; perturb >>= PERTURB_SHIFT) {
         i = (i << 2) + i + perturb + 1;
         ep = &ep0[i & mask];
-        if (ep->me_key == NULL
-                || ep->me_key == key
-                || (ep->me_hash == hash && mp->ma_keycmp(ep->me_key, key) == 0))
+        if (ep->key == NULL
+                || ep->key == key
+                || (ep->hash == hash && mp->keycmp(ep->key, key) == 0))
             return ep;
     }
     assert(0);          /* NOT REACHED */
@@ -106,20 +106,20 @@ insertdict_clean(PyDictObject *mp, PyObject *key, size_t hash,
                  PyObject *value) {
     size_t i;
     size_t perturb;
-    size_t mask = mp->ma_mask;
-    PyDictEntry *ep0 = mp->ma_table;
+    size_t mask = mp->mask;
+    PyDictEntry *ep0 = mp->table;
     PyDictEntry *ep;
     i = (size_t)hash & mask;
     ep = &ep0[i];
-    for (perturb = hash; ep->me_key != NULL; perturb >>= PERTURB_SHIFT) {
+    for (perturb = hash; ep->key != NULL; perturb >>= PERTURB_SHIFT) {
         i = (i << 2) + i + perturb + 1;
         ep = &ep0[i & mask];
     }
-    mp->ma_fill++;
-    mp->ma_used++;
-    ep->me_key = key;
-    ep->me_hash = hash;
-    ep->me_value = value;
+    mp->fill++;
+    mp->used++;
+    ep->key = key;
+    ep->hash = hash;
+    ep->value = value;
 }
 
 /*
@@ -138,17 +138,17 @@ dict_resize(PyDictObject *mp, size_t minused) {
             newsize <<= 1)
         ;
     /* Get space for a new table. */
-    oldtable = mp->ma_table;
-    size_t is_oldtable_malloced = (oldtable != mp->ma_smalltable);
+    oldtable = mp->table;
+    size_t is_oldtable_malloced = (oldtable != mp->smalltable);
     if (newsize == PyDict_MINSIZE) {
         /* A large table is shrinking, or we can't get any smaller. */
-        newtable = mp->ma_smalltable;
+        newtable = mp->smalltable;
         if (newtable == oldtable) {
-            if (mp->ma_fill == mp->ma_used) {
+            if (mp->fill == mp->used) {
                 /* No dummies, so no point doing anything. */
                 return EXIT_SUCCESS;
             }
-            assert(mp->ma_fill > mp->ma_used);
+            assert(mp->fill > mp->used);
             memcpy(small_copy, oldtable, sizeof(small_copy));
             oldtable = small_copy;
         }
@@ -160,18 +160,18 @@ dict_resize(PyDictObject *mp, size_t minused) {
     /* Make the dict empty, using the new table. */
     assert(newtable != oldtable);
     memset(newtable, 0, sizeof(PyDictEntry)* newsize);
-    mp->ma_table = newtable;
-    mp->ma_mask = newsize - 1;
-    mp->ma_used = 0;
-    size_t i = mp->ma_fill;
-    mp->ma_fill = 0;
+    mp->table = newtable;
+    mp->mask = newsize - 1;
+    mp->used = 0;
+    size_t i = mp->fill;
+    mp->fill = 0;
     for (ep = oldtable; i > 0; ep++) {
-        if (ep->me_value != NULL) {             /* active entry */
+        if (ep->value != NULL) {             /* active entry */
             --i;
-            insertdict_clean(mp, ep->me_key, ep->me_hash, ep->me_value);
-        } else if (ep->me_key != NULL) {    /* dummy entry */
+            insertdict_clean(mp, ep->key, ep->hash, ep->value);
+        } else if (ep->key != NULL) {    /* dummy entry */
             --i;
-            assert(ep->me_key == dummy);
+            assert(ep->key == dummy);
         }
         /* else key == value == NULL:  nothing to do */
     }
@@ -183,25 +183,25 @@ dict_resize(PyDictObject *mp, size_t minused) {
 /*helper function for sorting a PyDictEntry by its value*/
 static int
 _valcmp(const void *ep1, const void *ep2) {
-    return *(size_t *)(*(PyDictEntry *)ep1).me_value > *(size_t *)(*
-            (PyDictEntry *)ep2).me_value ? -1 : 1;
+    return *(size_t *)(*(PyDictEntry *)ep1).value > *(size_t *)(*
+            (PyDictEntry *)ep2).value ? -1 : 1;
 }
 
 PyDictObject *
-dict_new_custom(size_t ma_size,
-                size_t (*ma_keyhash)(PyObject *key),
-                int (*ma_keycmp)(PyObject *key1, PyObject *key2),
-                PyObject * (*ma_keydup)(PyObject *key),
-                PyObject * (*ma_valuedup)(PyObject *value),
-                PyObject * (*ma_default)(void),
-                void (*ma_keyfree)(PyObject *key),
-                void (*ma_valuefree)(PyObject *value)) {
+dict_cnew(size_t size,
+          size_t (*keyhash)(PyObject *key),
+          int (*keycmp)(PyObject *key1, PyObject *key2),
+          PyObject * (*keydup)(PyObject *key),
+          PyObject * (*valuedup)(PyObject *value),
+          PyObject * (*valuedefault)(void),
+          void (*keyfree)(PyObject *key),
+          void (*valuefree)(PyObject *value)) {
     PyDictObject *mp = (PyDictObject *)malloc(sizeof(PyDictObject));
     if (mp == NULL)
         return NULL;
     size_t newsize;
     for (newsize = PyDict_MINSIZE;
-            newsize < ma_size && newsize > 0;
+            newsize < size && newsize > 0;
             newsize <<= 1)
         ;
     if (newsize > PyDict_MINSIZE) {
@@ -209,19 +209,19 @@ dict_new_custom(size_t ma_size,
         if (newtable == NULL)
             return NULL;
         memset(newtable, 0, sizeof(PyDictEntry)* newsize);
-        mp->ma_table = newtable;
-        mp->ma_mask = newsize - 1;
-        mp->ma_fill = mp->ma_used = 0;
+        mp->table = newtable;
+        mp->mask = newsize - 1;
+        mp->fill = mp->used = 0;
     } else {
         EMPTY_TO_MINSIZE(mp);
     }
-    mp->ma_keyhash = ma_keyhash ? ma_keyhash : keyhash;
-    mp->ma_keycmp = ma_keycmp ? ma_keycmp : keycmp;
-    mp->ma_keydup = ma_keydup ? ma_keydup : keydup;
-    mp->ma_valuedup = ma_valuedup ? ma_valuedup : valuedup;
-    mp->ma_default = ma_default ? ma_default : get_default_value;
-    mp->ma_keyfree = ma_keyfree ? ma_keyfree : free;
-    mp->ma_valuefree = ma_valuefree ? ma_valuefree : free;
+    mp->keyhash = keyhash ? keyhash : default_keyhash;
+    mp->keycmp = keycmp ? keycmp : default_keycmp;
+    mp->keydup = keydup ? keydup : default_keydup;
+    mp->valuedup = valuedup ? valuedup : default_valuedup;
+    mp->valuedefault = valuedefault ? valuedefault : default_valuedefault;
+    mp->keyfree = keyfree ? keyfree : free;
+    mp->valuefree = valuefree ? valuefree : free;
     return mp;
 }
 
@@ -231,22 +231,24 @@ dict_new(void) {
     if (mp == NULL)
         return NULL;
     EMPTY_TO_MINSIZE(mp);
-    mp->ma_keyhash = keyhash;
-    mp->ma_keycmp = keycmp;
-    mp->ma_keydup = keydup;
-    mp->ma_valuedup = valuedup;
-    mp->ma_default = get_default_value;
+    mp->keyhash = default_keyhash;
+    mp->keycmp = default_keycmp;
+    mp->keydup = default_keydup;
+    mp->valuedup = default_valuedup;
+    mp->valuedefault = default_valuedefault;
+    mp->keyfree = free;
+    mp->valuefree = free;
     return mp;
 }
 
 void
 dict_clear(PyDictObject *mp) {
-    PyDictEntry *ep, *table = mp->ma_table;
+    PyDictEntry *ep, *table = mp->table;
     assert(table != NULL);
     PyDictEntry small_copy[PyDict_MINSIZE];
-    size_t table_is_malloced = (table != mp->ma_smalltable);
-    size_t fill = mp->ma_fill;
-    size_t used = mp->ma_used;
+    size_t table_is_malloced = (table != mp->smalltable);
+    size_t fill = mp->fill;
+    size_t used = mp->used;
     if (table_is_malloced)
         EMPTY_TO_MINSIZE(mp);
     else if (fill > 0) {
@@ -258,43 +260,81 @@ dict_clear(PyDictObject *mp) {
         return;
     for (ep = table; used > 0; ep++) {
         /*only free active entry, this is different from Python 2.7*/
-        if (ep->me_value) {
+        if (ep->value) {
             used--;
-            mp->ma_keyfree(ep->me_key);
-            mp->ma_valuefree(ep->me_value);
+            mp->keyfree(ep->key);
+            mp->valuefree(ep->value);
         }
     }
     if (table_is_malloced)
         free(table);
 }
 
+void
+dict_free(PyDictObject *mp) {
+    dict_clear(mp);
+    free(mp);
+}
+
 PyObject *
-dict_search(PyDictObject *mp, PyObject *key) {
+dict_get(PyDictObject *mp, PyObject *key) {
     assert(key);
-    size_t hash = mp->ma_keyhash(key);
+    size_t hash = mp->keyhash(key);
     PyDictEntry *ep = lookdict(mp, key, hash);
-    return ep->me_value;
+    return ep->value;
+}
+
+int
+dict_set(PyDictObject *mp, PyObject *key, PyObject *value) {
+    assert(key);
+    assert(value);
+    size_t hash = mp->keyhash(key);
+    PyDictEntry *ep = lookdict(mp, key, hash);
+    /*if key exists, same as dict_update*/
+    if (ep->value) {
+        PyObject *old_value = ep->value;
+        if ((ep->value = mp->valuedup(value)) == NULL)
+            return EXIT_FAILURE;
+        mp->valuefree(old_value);
+        return EXIT_SUCCESS;
+    } else {
+        /*if key doesn't exist, same as dict_add*/
+        PyObject *old_key = ep->key;
+        if ((ep->key = mp->keydup(key)) == NULL)
+            return EXIT_FAILURE;
+        if ((ep->value = mp->valuedup(value)) == NULL) {
+            mp->keyfree(ep->key);
+            return EXIT_FAILURE;
+        }
+        if (old_key == NULL)
+            mp->fill++;
+        mp->used++;
+        ep->hash = hash;
+        if (NEED_RESIZE(mp))
+            return dict_resize(mp, RESIZE_NUM(mp));
+        return EXIT_SUCCESS;
+    }
 }
 
 int
 dict_add(PyDictObject *mp, PyObject *key, PyObject *value) {
     assert(key);
     assert(value);
-    size_t hash = mp->ma_keyhash(key);
+    size_t hash = mp->keyhash(key);
     PyDictEntry *ep = lookdict(mp, key, hash);
     /*only for non-existing keys*/
-    assert(ep->me_value == NULL);
-    PyObject *old_key = ep->me_key;
-    if ((ep->me_key = mp->ma_keydup(key)) == NULL)
+    assert(ep->value == NULL);
+    PyObject *old_key = ep->key;
+    if ((ep->key = mp->keydup(key)) == NULL)
         return EXIT_FAILURE;
-    if ((ep->me_value = mp->ma_valuedup(value)) == NULL) {
-        mp->ma_keyfree(ep->me_key);
+    if ((ep->value = mp->valuedup(value)) == NULL) {
+        mp->keyfree(ep->key);
         return EXIT_FAILURE;
     }
     if (old_key == NULL)
-        mp->ma_fill++;
-    mp->ma_used++;
-    ep->me_hash = hash;
+        mp->fill++;
+    mp->used++;
+    ep->hash = hash;
     if (NEED_RESIZE(mp))
         return dict_resize(mp, RESIZE_NUM(mp));
     return EXIT_SUCCESS;
@@ -304,65 +344,59 @@ int
 dict_update(PyDictObject *mp, PyObject *key, PyObject *value) {
     assert(key);
     assert(value);
-    size_t hash = mp->ma_keyhash(key);
+    size_t hash = mp->keyhash(key);
     PyDictEntry *ep = lookdict(mp, key, hash);
     /*only for existing keys*/
-    assert(ep->me_value != NULL);
-    PyObject *old_value = ep->me_value;
-    if ((ep->me_value = mp->ma_valuedup(value)) == NULL)
+    assert(ep->value != NULL);
+    PyObject *old_value = ep->value;
+    if ((ep->value = mp->valuedup(value)) == NULL)
         return EXIT_FAILURE;
-    mp->ma_valuefree(old_value);
+    mp->valuefree(old_value);
     return EXIT_SUCCESS;
 }
 
-int
+void
 dict_del(PyDictObject *mp, PyObject *key) {
     assert(key);
-    size_t hash = mp->ma_keyhash(key);
+    size_t hash = mp->keyhash(key);
     PyDictEntry *ep = lookdict(mp, key, hash);
     /*only for existing keys*/
-    assert(ep->me_value != NULL);
-    mp->ma_keyfree(ep->me_key);
-    mp->ma_valuefree(ep->me_value);
-    ep->me_key = dummy;
-    ep->me_value = NULL;
-    mp->ma_used--;
-    return EXIT_SUCCESS;
-}
-
-size_t
-dict_contain(PyDictObject *mp, PyObject *key) {
-    return dict_search(mp, key) ? 1 : 0;
+    assert(ep->value != NULL);
+    mp->keyfree(ep->key);
+    mp->valuefree(ep->value);
+    ep->key = dummy;
+    ep->value = NULL;
+    mp->used--;
 }
 
 PyObject *
-dict_force_search(PyDictObject *mp, PyObject *key) {
+dict_fget(PyDictObject *mp, PyObject *key) {
     assert(key);
-    size_t hash = mp->ma_keyhash(key);
+    size_t hash = mp->keyhash(key);
     PyDictEntry *ep = lookdict(mp, key, hash);
-    if (ep->me_value == NULL) { /* dummy or unused */
-        PyObject *old_key = ep->me_key;
-        if ((ep->me_key = mp->ma_keydup(key)) == NULL)
+    if (ep->value == NULL) { /* dummy or unused */
+        PyObject *old_key = ep->key;
+        if ((ep->key = mp->keydup(key)) == NULL)
             return NULL;
-        if ((ep->me_value = mp->ma_default()) == NULL) {
-            mp->ma_keyfree(ep->me_key);
+        if ((ep->value = mp->valuedefault()) == NULL) {
+            mp->keyfree(ep->key);
             return NULL;
         }
         if (old_key == NULL)
-            mp->ma_fill++;
-        mp->ma_used++;
-        ep->me_hash = hash;
+            mp->fill++;
+        mp->used++;
+        ep->hash = hash;
         if (NEED_RESIZE(mp)) {
             dict_resize(mp, RESIZE_NUM(mp));
             ep = lookdict_nodummy(mp, key, hash);
         }
     }
-    return ep->me_value;
+    return ep->value;
 }
 
 size_t
 dict_len(PyDictObject *mp) {
-    return mp->ma_used;
+    return mp->used;
 }
 
 /*print key value pair by value DESC order*/
@@ -370,18 +404,18 @@ void
 dict_print_by_value_desc(PyDictObject *mp) {
     PyDictEntry *ep;
     PyDictEntry *temp_table = (PyDictEntry *)malloc(sizeof(PyDictEntry) *
-                              (mp->ma_used));
-    size_t i = 0, used = mp->ma_used;
-    for (ep = mp->ma_table; used > 0; ep++) {
-        if (ep->me_value) {
+                              (mp->used));
+    size_t i = 0, used = mp->used;
+    for (ep = mp->table; used > 0; ep++) {
+        if (ep->value) {
             used--;
             temp_table[i++] = *ep;
         }
     }
-    used = mp->ma_used;
+    used = mp->used;
     qsort(temp_table, used, sizeof(temp_table[0]), _valcmp);
     for (i = 0; i < used; i++)
-        fprintf(stdout, "%s\t%d\n", (char *)temp_table[i].me_key,
-                *(size_t *)temp_table[i].me_value);
+        fprintf(stdout, "%s\t%d\n", (char *)temp_table[i].key,
+                *(size_t *)temp_table[i].value);
     free(temp_table);
 }
