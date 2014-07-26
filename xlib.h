4b9eea57e6b16927dc271f1f9f438d086fb58501
zhuanyenan <280145668@qq.com>
2014-07-26 23:04:31 +0800

    Update xlib.h
    unify dict and set macros and constants.

--- xlib.h
+++ xlib.h
@@ -3,8 +3,32 @@
 #include <stdlib.h>
 #include <stddef.h>
 #include <assert.h>
-typedef void PyObject;
 #include "dict.h"
 #include "rbtree.h"
 #include "list.h"
 #include "set.h"
+
+#ifndef X_DEBUG
+    #define X_DEBUG
+#endif
+
+#define PERTURB_SHIFT 5
+
+#define HASH_MINSIZE 8
+
+#define NEED_RESIZE(op) ((op)->fill * 3 >= ((op)->mask + 1) * 2)
+
+#define RESIZE_NUM(op) (((op)->used > 50000 ? 2 : 4) * (op)->used)
+
+#define INIT_NONZERO_DICT_SLOTS(op) do {                                \
+    (op)->table = (op)->smalltable;                               \
+    (op)->mask = HASH_MINSIZE - 1;                                 \
+    } while(0)
+
+#define EMPTY_TO_MINSIZE(op) do {                                       \
+    memset((op)->smalltable, 0, sizeof((op)->smalltable));        \
+    (op)->used = (op)->fill = 0;                                  \
+    INIT_NONZERO_DICT_SLOTS(op);                                        \
+    } while(0)
+
+typedef struct {} DummyStruct;
