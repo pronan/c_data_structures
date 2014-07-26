dd291b03066525ca9f43e60e15b967989f3bd37d
zhuanyenan <280145668@qq.com>
2014-07-26 23:07:28 +0800

    Update dict.h

--- dict.h
+++ dict.h
@@ -1,57 +1,58 @@
-#define PyDict_MINSIZE 8
-#define PERTURB_SHIFT 5
-#define NEED_RESIZE(mp) ((mp)->fill * 3 >= ((mp)->mask + 1) * 2)
-#define RESIZE_NUM(mp) (((mp)->used > 50000 ? 2 : 4) * (mp)->used)
-#define INIT_NONZERO_DICT_SLOTS(mp) do {                                \
-    (mp)->table = (mp)->smalltable;                               \
-    (mp)->mask = PyDict_MINSIZE - 1;                                 \
-    } while(0)
-
-#define EMPTY_TO_MINSIZE(mp) do {                                       \
-    memset((mp)->smalltable, 0, sizeof((mp)->smalltable));        \
-    (mp)->used = (mp)->fill = 0;                                  \
-    INIT_NONZERO_DICT_SLOTS(mp);                                        \
-    } while(0)
+#define DICT_COPY_INIT(mp) dict_cnew(\
+    (mp)->used,\
+    (mp)->keyhash,\
+    (mp)->keycmp,\
+    (mp)->keydup,\
+    (mp)->valuedup,\
+    (mp)->valuedefault,\
+    (mp)->keyfree,\
+    (mp)->valuefree);
 
 typedef struct {
     size_t hash;
-    PyObject *key;
-    PyObject *value;
-} PyDictEntry;
+    void *key;
+    void *value;
+} DictEntry;
 
-typedef struct _dictobject PyDictObject;
+typedef struct _dictobject DictObject;
 struct _dictobject {
     size_t fill;  /* # Active + # Dummy */
     size_t used;  /* # Active */
     size_t mask;
-    PyDictEntry *table;
-    PyDictEntry smalltable[PyDict_MINSIZE];
-    size_t (*keyhash)(PyObject *key);
-    int (*keycmp)(PyObject *key1, PyObject *key2);
-    PyObject *(*keydup)(PyObject *key);
-    PyObject *(*valuedup)(PyObject *value);
-    PyObject *(*valuedefault)(void);
-    void (*keyfree)(PyObject *key);
-    void (*valuefree)(PyObject *value);
+    DictEntry *table;
+    DictEntry smalltable[HASH_MINSIZE];
+    size_t (*keyhash)(void *key);
+    int (*keycmp)(void *key1, void *key2);
+    void *(*keydup)(void *key);
+    void *(*valuedup)(void *value);
+    void *(*valuedefault)(void);
+    void (*keyfree)(void *key);
+    void (*valuefree)(void *value);
 };
 
-PyDictObject *
+DictObject *
 dict_cnew(size_t size,
-          size_t (*keyhash)(PyObject *key),
-          int (*keycmp)(PyObject *key1, PyObject *key2),
-          PyObject * (*keydup)(PyObject *key),
-          PyObject * (*valuedup)(PyObject *value),
-          PyObject * (*valuedefault)(void),
-          void (*keyfree)(PyObject *key),
-          void (*valuefree)(PyObject *value));
-PyDictObject *dict_new(void);
-void dict_clear(PyDictObject *mp);
-void dict_free(PyDictObject *mp);
-PyObject *dict_get(PyDictObject *mp, PyObject *key);
-PyObject *dict_fget(PyDictObject *mp, PyObject *key);
-int dict_set(PyDictObject *mp, PyObject *key, PyObject *value);
-void dict_del(PyDictObject *mp, PyObject *key);
-int dict_add(PyDictObject *mp, PyObject *key, PyObject *value);
-int dict_update(PyDictObject *mp, PyObject *key, PyObject *value);
-size_t dict_len(PyDictObject *mp);
-void dict_print_by_value_desc(PyDictObject *mp);
+          size_t (*keyhash)(void *key),
+          int (*keycmp)(void *key1, void *key2),
+          void * (*keydup)(void *key),
+          void * (*valuedup)(void *value),
+          void * (*valuedefault)(void),
+          void (*keyfree)(void *key),
+          void (*valuefree)(void *value));
+DictObject *dict_new(void);
+void dict_clear(DictObject *mp);
+void dict_free(DictObject *mp);
+int dict_update(DictObject *mp, DictObject *other);
+DictObject *dict_copy(DictObject *mp);
+void *dict_get(DictObject *mp, void *key);
+int dict_set(DictObject *mp, void *key, void *value);
+void *dict_fget(DictObject *mp, void *key);
+void dict_del(DictObject *mp, void *key);
+int dict_add(DictObject *mp, void *key, void *value);
+int dict_update_value(DictObject *mp, void *key, void *value);
+int dict_rset(DictObject *mp, void *key, void *value);
+int dict_radd(DictObject *mp, void *key, void *value);
+int dict_rupdate_value(DictObject *mp, void *key, void *value);
+size_t dict_has(DictObject *mp, void *key);
+size_t dict_len(DictObject *mp);
+void dict_print_by_value_desc(DictObject *mp);
