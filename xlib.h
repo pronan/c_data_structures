c03f83cb54c8e2a5fd3f925596b44c9bdf94a64a
zhuanyenan <280145668@qq.com>
2014-07-29 21:53:13 +0800

    Update xlib.h
    add list.c related macros

--- xlib.h
+++ xlib.h
@@ -5,8 +5,29 @@
 #include <assert.h>
 
 #ifndef X_DEBUG
-    #define X_DEBUG
+#define X_DEBUG
 #endif
+
+#define Py_SIZE(self) ((self)->used)
+
+#define PY_SIZE_MAX ((size_t)-1)
+
+#define PY_SSIZE_T_MAX ((ssize_t)(((size_t)-1)>>1))
+
+#define PY_SSIZE_T_MIN (-PY_SSIZE_T_MAX-1)
+
+#define PyMem_MALLOC(n) ((size_t)(n)>(size_t)PY_SSIZE_T_MAX?\
+                          NULL:malloc((n)?(n):1))
+
+#define PyMem_REALLOC(p, n) ((size_t)(n)>(size_t)PY_SSIZE_T_MAX ?\
+                             NULL:realloc((p),(n)?(n):1))
+#define PyMem_FREE free
+
+#define PyMem_NEW(type, n) (((size_t)(n)>PY_SSIZE_T_MAX/sizeof(type))?\
+                            NULL:((type*)PyMem_MALLOC((n)*sizeof(type))))
+
+#define PyMem_RESIZE(p, type, n) ((p)=((size_t)(n)>PY_SSIZE_T_MAX/sizeof(type))?\
+                                  NULL:(type*) PyMem_REALLOC((p),(n)*sizeof(type)))
 
 #define PERTURB_SHIFT 5
 
@@ -27,7 +48,33 @@
     INIT_NONZERO_DICT_SLOTS(op);                                        \
     } while(0)
 
+#define iter(op) ((op)->type==LIST?list_iter_new(op):\
+                  (op)->type==DICT?dict_iter_new(op):\
+                  (op)->type==SET?set_iter_new(op):\
+                  NULL)
+
+#define iterw(iop,keyaddr) ((iop)->type==LIST?list_iter_walk(iop,keyaddr):\
+                            (iop)->type==DICT?dict_iter_walk(iop,keyaddr):\
+                            (iop)->type==SET?set_iter_walk(iop,keyaddr):\
+                            0)
+
+#define iterf(iop) ((iop)->type==LIST?list_iter_flush(iop):\
+                    (iop)->type==DICT?dict_iter_flush(iop):\
+                    (iop)->type==SET?set_iter_flush(iop):\
+                    0)
+
 typedef struct {} DummyStruct;
+
+typedef enum {
+    LIST, DICT, SET
+} ObjectType;
+
+typedef struct {
+    ObjectType type;
+    void *object;
+    void *inipos;
+    size_t rest;
+} IterObject;
 
 #include "dict.h"
 #include "rbtree.h"
