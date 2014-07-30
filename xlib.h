e6249565701003aa5c6bb7d5597389256032eb78
zhuanyenan <280145668@qq.com>
2014-07-30 12:36:59 +0800

    Update xlib.h

--- xlib.h
+++ xlib.h
@@ -5,29 +5,35 @@
 #include <assert.h>
 
 #ifndef X_DEBUG
-#define X_DEBUG
+    #define X_DEBUG
 #endif
 
-#define Py_SIZE(self) ((self)->used)
+#ifndef SIZE_MAX
+    #define SIZE_MAX ((size_t)-1)
+#endif
 
-#define PY_SIZE_MAX ((size_t)-1)
+#define SSIZE_T_MAX ((ssize_t)(((size_t)-1)>>1))
 
-#define PY_SSIZE_T_MAX ((ssize_t)(((size_t)-1)>>1))
+#define SSIZE_T_MIN (-SSIZE_T_MAX-1)
 
-#define PY_SSIZE_T_MIN (-PY_SSIZE_T_MAX-1)
-
-#define PyMem_MALLOC(n) ((size_t)(n)>(size_t)PY_SSIZE_T_MAX?\
+/* Mem_MALLOC(0) means malloc(1). Some systems would return NULL
+   for malloc(0), which would be treated as an error. Some platforms
+   would return a pointer with no memory behind it, which would break
+   pymalloc. To solve these problems, allocate an extra byte. */
+/* Returns NULL to indicate error if a negative size or size larger than
+   ssize_t can represent is supplied.  Helps prevents security holes. */
+#define Mem_MALLOC(n) ((size_t)(n)>(size_t)SSIZE_T_MAX?\
                           NULL:malloc((n)?(n):1))
 
-#define PyMem_REALLOC(p, n) ((size_t)(n)>(size_t)PY_SSIZE_T_MAX ?\
+#define Mem_REALLOC(p, n) ((size_t)(n)>(size_t)SSIZE_T_MAX ?\
                              NULL:realloc((p),(n)?(n):1))
-#define PyMem_FREE free
+#define Mem_FREE free
 
-#define PyMem_NEW(type, n) (((size_t)(n)>PY_SSIZE_T_MAX/sizeof(type))?\
-                            NULL:((type*)PyMem_MALLOC((n)*sizeof(type))))
+#define Mem_NEW(type, n) (((size_t)(n)>SSIZE_T_MAX/sizeof(type))?\
+                            NULL:((type*)Mem_MALLOC((n)*sizeof(type))))
 
-#define PyMem_RESIZE(p, type, n) ((p)=((size_t)(n)>PY_SSIZE_T_MAX/sizeof(type))?\
-                                  NULL:(type*) PyMem_REALLOC((p),(n)*sizeof(type)))
+#define Mem_RESIZE(p, type, n) ((p)=((size_t)(n)>SSIZE_T_MAX/sizeof(type))?\
+                                  NULL:(type*) Mem_REALLOC((p),(n)*sizeof(type)))
 
 #define PERTURB_SHIFT 5
 
