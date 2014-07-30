03b647752595d1e4791925ac7146ae01403b0866
zhuanyenan <280145668@qq.com>
2014-07-30 13:27:27 +0800

    Update rbtree.h
    unify names and types

--- rbtree.h
+++ rbtree.h
@@ -1,49 +1,60 @@
 typedef enum {
     BLACK, RED
-} rb_color;
+} rbcolor;
 
 typedef struct rbnode {
-    rb_color color;
+    rbcolor color;
     struct rbnode *p;
     struct rbnode *left;
     struct rbnode *right;
-    PyObject *key;
-    PyObject *value;
+    void *key;
+    void *value;
 } rbnode;
 
 typedef struct rbtree {
     rbnode *root;
     rbnode *nil;
     size_t size;
-    int (*keycmp)(PyObject *key1, PyObject *key2);
-    PyObject *(*keydup)(PyObject *key);
-    PyObject *(*valuedup)(PyObject *value);
-    PyObject *(*valuedefault)(void);
-    void (*keyfree)(PyObject *key);
-    void (*valuefree)(PyObject *value);
+    int (*keycmp)(void *key1, void *key2);
+    void *(*keydup)(void *key);
+    void *(*valuedup)(void *value);
+    void *(*valuedefault)(void);
+    void (*keyfree)(void *key);
+    void (*valuefree)(void *value);
 } rbtree;
 
 typedef struct {
-    PyObject *key;
-    PyObject *value;
+    void *key;
+    void *value;
 } key_value;
 
+/* tree level functions */
 rbtree *
-rb_cnew(int (*keycmp)(PyObject *key1, PyObject *key2),
-        PyObject * (*keydup)(PyObject *key),
-        PyObject * (*valuedup)(PyObject *value),
-        PyObject * (*valuedefault)(void),
-        void (*keyfree)(PyObject *key),
-        void (*valuefree)(PyObject *value));
+rb_cnew(int (*keycmp)(void *key1, void *key2),
+        void * (*keydup)(void *key),
+        void * (*valuedup)(void *value),
+        void * (*valuedefault)(void),
+        void (*keyfree)(void *key),
+        void (*valuefree)(void *value));
 rbtree *rb_new(void) ;
 void rb_clear(rbtree *tr) ;
 void rb_free(rbtree *tr) ;
-PyObject *rb_get(rbtree *tr, PyObject *key) ;
-PyObject *rb_fget(rbtree *tr, PyObject *key) ;
-int rb_set(rbtree *tr, PyObject *key, PyObject *value) ;
-void rb_del(rbtree *tr, PyObject *key) ;
-int rb_add(rbtree *tr, PyObject *key, PyObject *value) ;
-int rb_update(rbtree *tr, PyObject *key, PyObject *value) ;
+
+/* node level functions */
+void *rb_get(rbtree *tr, void *key) ;
+void *rb_fget(rbtree *tr, void *key) ;
+int rb_set(rbtree *tr, void *key, void *value) ;
+int rb_add(rbtree *tr, void *key, void *value) ;
+int rb_update(rbtree *tr, void *key, void *value) ;
+void rb_del(rbtree *tr, void *key) ;
 rbnode *rb_min(rbtree *tr, rbnode* x) ;
 rbnode *rb_max(rbtree *tr, rbnode* x) ;
+
+/*node level functions, passing references instead of a copy.
+'r' prefix is short for 'reference'*/
+int rb_rset(rbtree *tr, void *key, void *value) ;
+int rb_radd(rbtree *tr, void *key, void *value) ;
+int rb_rupdate(rbtree *tr, void *key, void *value) ;
+
+/*other functions for printing or testing*/
 void rb_print(rbtree *tr);
